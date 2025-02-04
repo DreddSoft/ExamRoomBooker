e79a0fa86b93ffcc02d2ebbed8dab42c3ae531b6
DreddSoft <abonilla2093@gmail.com>
2025-02-04 16:57:23 +0100

    Se crean los archivos principales de la aplicación

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php require_once "_header.php"; ?>
+
+    <main>
+        
+    </main>
+
+    <?php require_once "_footer.php"; ?>
+    
+</body>
+</html>
