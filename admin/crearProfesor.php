1d1d73bb97a2498ae4f272a135326de8f64ea8bc
davix1997 <davixville@gmail.com>
2025-02-13 19:49:14 +0100

    finalizacion del archivo crearProfesor, mostrarProfesor a medio hacer y creacion de archivo .js

--- admin/crearProfesor.php
+++ admin/crearProfesor.php
@@ -2,50 +2,48 @@
     //dar por hecho que el booleano activo es 1 y el booleano admin es 0
     //comprobar tambien que el usuario que esta conectado es el admin antes de hcer nada(usando la bd), sino redirigir al index.php
     //poner en el main, no hacer estilos
+    //inicio sesion
     session_start();
-
-
-    //compruebo si hay un profesor conectado, so no lo mando al login
+    //ESTO ES PARA PRUEBA, BORRAR LUEGO!!!!!!!!!!!
+    $_SESSION["id"] = 11;
+    //compruebo si hay un profesor conectado, si no lo mando al login
     if(!isset($_SESSION["id"])){
         header("Location:../login.php");
     }
-    require_once($_SERVER['DOCUMENT_ROOT'] . '/clases/bd.class.php');
+    //requiero la clase bd
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/examroombooker/clases/bd.class.php');
     $bd = new BD();
-    
+    //conecto a la base de datos
     $conexion = $bd->abrirConexion();
-    //compruebo que el método es post
-    
-    if($_SERVER["REQUEST_METHOD"] === "POST"){
-    //id del prof que usare en la co1nsulta
-        $idAdmin = $_SESSION["id"];
+    //guardo el id de sesion en una variable para usarlo luego
+    $idAdmin = $_SESSION["id"];
        
         //consulta para sacar si el profesor conectado es admin o no
         $query1 = "
             SELECT admin FROM profesores
             where id = '$idAdmin';
         ";
+        $resultado = $bd->capturarDatos($query1);
 
-        $resultado = mysqli_query($conexion, $query1);
-
-        while($registro = mysqli_fetch_assoc($resultado)){
-            $esAdmin = $registro["admin"];
-        }
         //si no es admin le mando al index
-        if($esAdmin == false){
+        if($resultado[0]["admin"] == 0){
             header("Location:../index.php");
-        //si lo es hago un insert into con los datos del form
-        //}else{//el 1 es el campo activo y el 0 el campo admin
-        $id = $_POST['id'];
-        $usuario = $_POST['usuario'];
-        $passw = $_POST['passw'];
-        $nombre = $_POST['nombre'];
-        $ape1 = $_POST['ape1'];
-        $ape2 = $_POST['ape2'];
-        $email = $_POST['email'];
+    //compruebo que el método es post
+    if($_SERVER["REQUEST_METHOD"] === "POST"){
+        //guardo los datos que me ha pasado el usuario en variables, que voy a usar en la consulta
+        $id = htmlspecialchars($_POST['id']);
+        $usuario = htmlspecialchars($_POST['usuario']);
+        $passw = htmlspecialchars($_POST['passw']);
+        $nombre = htmlspecialchars($_POST['nombre']);
+        $ape1 = htmlspecialchars($_POST['ape1']);
+        $ape2 = htmlspecialchars($_POST['ape2']);
+        $email = htmlspecialchars($_POST['email']);
+        //consulta para insertar en la tabla profesores un nuevo profesor con los datos indcados
         $query2 = "
                 INSERT INTO profesores (id, usuario, passw, nombre, ape1, ape2, activo, email, admin)
                 VALUES ('$id', '$usuario', '$passw', '$nombre', '$ape1', '$ape2', 1, '$email', 0);
             ";
+            //uso el metodo insertar datos de la clase bd
             $bd->insertarDatos($query2);
             //cierro la conexion con la bd
             $bd->cerrarConexion();
