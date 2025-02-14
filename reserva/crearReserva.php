cd7dbe617342755c788c23d2f95599141651670b
danielgr29 <danielgr2903@gmail.com>
2025-02-14 21:33:40 +0100

    Principio de crearReserva

--- reserva/crearReserva.php
+++ reserva/crearReserva.php
@@ -1,63 +1,56 @@
+<?php
+session_start();
 
-<!-- funcion para insertar datos, para select y para delete,
-Funcion inserta con los id del porfesor y de la asignatura, usando los de la base de datos con un select  -->
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    $usuarioIntroducido = htmlspecialchars($_GET["idProfesor"]);
+    try {
+
+        $bd->abrirConexion();
+
+        $sql = "SELECT * FROM Profesores";
 
 
+        
+
+
+
+
+
+        //pasar variable por parametro para hacer la consulta
+        $datos = $bd->capturarDatos($sql);
+
+        foreach ($datos as $profesor) {
+            echo $profesor['id'] . " - " . $profesor['usuario'] . " | " . $profesor['nombre'] . "<br>";
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    } finally {
+        $bd->cerrarConexion();
+    }
+
+
+    if (empty($usuarioIntroducido)) {
+        $mensaje = "Debes rellenar todos los campos";
+    }
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formualrio</title>
 </head>
-<body>
 
+<body>
+    <!-- funcion para insertar datos, para select y para delete,
+Funcion inserta con los id del porfesor y de la asignatura, usando los de la base de datos con un select  -->
     <form action="crearReserva.php" method="post">
         <label for="consulta" name="consulta"> Ingrese el id del profesor para ver la reserva: </label><input type="text" name="consulta">
         <input type="submit" value="Enviar">
     </form>
-    
 </body>
+
 </html>
-
-<?php
-
-
-
-if($_SERVER["REQUEST_METHOD"] === "POST"){
-    $consulta=htmlspecialchars($_REQUEST["id"]);
-    $hots="localhost";
-    $user="root";
-    $pass="";
-    $name_db="examroombooker";
-
-
-    //Establer conexion
-    $conexion= mysqli_connect($hots,$user,$pass,$name_db);
-
-
-    //Crear y ejecutar consultas
-    $query1 = "SELECT id,descripcion,numAlumnos FROM reservas WHERE id='$consulta'";
-    $resultado = mysqli_query($conexion, $query1);
-
-
-    echo "  <h1>Asignatura DE consulta DE " .$consulta."</h1>";
-    //Para ver resultados se usan, mysqli_fetch_row() ó mysqli_fetch_assoc()
-    while ($registro = mysqli_fetch_assoc($resultado)) {
-        echo "
-      
-        Id de la asignatura:  ".$registro["id"]. "<br>"." consulta: ".$registro["nombre"]. "<br>"." IdDepartamento: ".$registro["idDepartamento"] . "<br><br><br>";
-        
-    }
-
-
-
-    function obtenerReservasId(): void{
-        
-    }
-
-}
-
-
-?>
